c5d27f9ddfeee13d9f4545e78d1e5d689d372ced
kali <kali@kali.kali>
2021-06-30 15:18:02 +0300

    Refactor code in respect to PEP8

--- pseudonyms.py
+++ pseudonyms.py
@@ -1,24 +1,34 @@
-import sys, random
-print("Ласкаво просимо в 'Підбір псевдоніма для Івана'\n")
-print("Псевдонім Івана буде:\n\n")
+"""This script pickes up a random funny Ukrainian pseudonym"""
 
-first = ('Геть', 'Проб', 'Міто', 'Тутан', 'Найшлях', 'Дуд', 'Пуц',
-         'Провин', 'Недалек', 'Близ', 'Травкен', 'Перемут')
 
-last = ('Куцько', 'Додько', 'Паликрай', 'Мітко', 'Забирайко',
-        'Недавайко', 'Посилайко', 'Важницький', 'Тамєбік', 'Грішко',
-        'Наливайко', 'Шмальницький', 'Конопляник', 'Дідок')
+import sys
+import random
 
-while True:
-    firstName = random.choice(first)
 
-    lastName = random.choice(last)
+def main():
+        print("Ласкаво просимо в 'Підбір псевдоніма для Івана'\n")
+        print("Псевдонім Івана буде:\n\n")
 
-    print("\n\n")
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
-    try_again = input("\n\nСпробуєш ще? (Натисни Enter або n, щоб вийти)\n")
-    if try_again.lower() == "n":
-        break
+        first = ('Геть', 'Проб', 'Міто', 'Тутан', 'Найшлях', 'Дуд', 'Пуц',
+                'Провин', 'Недалек', 'Близ', 'Травкен', 'Перемут')
 
-input("\nНажміть Enter для завершення роботи.")
+        last = ('Куцько', 'Додько', 'Паликрай', 'Мітко', 'Забирайко',
+                'Недавайко', 'Посилайко', 'Важницький', 'Тамєбік', 'Грішко',
+                'Наливайко', 'Шмальницький', 'Конопляник', 'Дідок')
+
+        while True:
+                firstName = random.choice(first)
+
+                lastName = random.choice(last)
+
+                print("\n\n")
+                print("{} {}".format(firstName, lastName), file=sys.stderr)
+                print("\n\n")
+                try_again = input("\n\nСпробуєш ще? (Натисни Enter або n, щоб вийти)\n")
+                if try_again.lower() == "n":
+                        break
+
+        input("\nНажміть Enter для завершення роботи.")
+
+if __name__ == "__main__":
+        main()
